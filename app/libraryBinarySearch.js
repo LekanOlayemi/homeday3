b1e4b63f15afc16e114299e2f95ecd0c222dab0a
lekanolayemi <lexzikan@gmail.com>
2017-01-11 11:51:32 +0100

    added libraryBinarySearch.js

---
+++ app/libraryBinarySearch.js
@@ -0,0 +1,41 @@
+function oneToTwenty(){
+function twoToForty(){
+	function tentoOneThousand(){
+
+ oneToTwenty.prototype.toTwenty() = function(){
+
+	var start = 1;
+	var end = 20;
+	var oneToTwenty = [];
+
+	for(var i = start; i <= end; i+= 1) {
+		oneToTwenty.push(i);
+	}
+	return oneToTwenty;
+}
+  twoToForty.prototype.toForty() = function(){
+
+var start = 2;
+	var end = 40;
+	var twoToForty = [];
+
+	for(var i = start; i <= end; i+= 1) {
+		twoToForty.push(i);
+	}
+	return twoToForty;
+}
+
+  tenToOneThousand.prototype.toOneThousand() = function(){
+
+var start = 10;
+	var end = 1000;
+	var tenToThousand = [];
+
+	for(var i = start; i <= end; i+= 1) {
+		tenToThousand.push(i);
+	}
+	return ToThousand;
+    }
+}  
+}
+}
